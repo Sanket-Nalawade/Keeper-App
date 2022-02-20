d17720cb99edf61c34dc31e97a8951edca347bbe
Sanket Suresh Nalawade <91829561+Sanket-Nalawade@users.noreply.github.com>
2022-02-20 14:39:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./header";
+import Note from "./note";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
